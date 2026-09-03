dbb244920b5c4162d41b3fc05d4aa54325fcb792
ogedz <ogedengbe_m@yahoo.com>
2026-09-03 12:51:16 +0100

    fixing server error

--- servealways/server.js
+++ servealways/server.js
@@ -1,13 +1,14 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { dirname } from 'path';
 
 // Create __dirname equivalent for ES modules
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 /**
  * Configure Express middleware
@@ -20,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 // Tell Express where to find your templates
-app.set('views', path.join(__dirname, 'src/views'));
+app.set('views', path.join(__dirname, 'src', 'views'));
 
 /**
  * Routes
@@ -42,5 +43,6 @@ app.get('/projects', async (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
+    console.log(`Views directory: ${path.join(__dirname, 'src', 'views')}`);
 });
